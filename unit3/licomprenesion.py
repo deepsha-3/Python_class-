806bb49ddec6017fd4350ff97bc48f5c0c5045c3
deepsha-3 <itsmedeepsha375@gmail.com>
2025-06-23 06:52:29 +0545

    new file added

--- unit3/licomprenesion.py
+++ unit3/licomprenesion.py
@@ -10,7 +10,14 @@ letters = ['a', 'b', 'c', 'd']
 upper=[ch.upper() for ch in letters]
 print("Uppercase List:", upper)
 
+
 # even number comprehension
-nums=[1,2,3,4,5 ,6 ,7,8]
+nums=[1,2,3,4,5 ,6 ,7]
 evens=[x for x in nums if x % 2 ==0]
-print("Even Numbers List:", evens)
+print("Even Numbers List even", evens)
+
+
+# odd numbers comperehension
+nums=[1,2,3,4,5 ,6 ,7,8]
+odds=[x for x in nums if x %2 ==1]
+print("Even Numbers List odds", odds)
