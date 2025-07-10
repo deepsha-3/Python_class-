c92df30e092cf1e4aab97c0231292feb9d0f33e6
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-10 07:23:44 +0545

    new file

--- unit 4/init.py
+++ unit 4/init.py
@@ -4,6 +4,13 @@ class Car:
         self.color = color
 
 
+
 car1= Car("BMW", "Black")
 print("The car name is:", car1.name)
 print("The car color is:", car1.color )
+
+car2= Car("Audi", "Black")
+print("The car name is:", car2.name)
+print("The car color is:", car2.color )
+
+
---
+++ unit 4/int2.py
@@ -0,0 +1,22 @@
+
+class Computer:
+
+    def __init__(self, device_name, processor, device_id, system_type,edition):
+        self.device_name = device_name
+        self.procesor = processor
+        self.device_id = device_id
+        self.system_type = system_type
+        self.edition = edition
+
+com1 = Computer("Dell","i7","3C4F","64 bits Os"," 11 Pro" )
+com2 = Computer("Dell", "i5", "224F", "32 bits Os", "11"  )
+com3 = Computer("Dell","i3","554F","64 bits Os","10")
+
+print(f"This is first computer details:", com1.device_name,com1.procesor, com1.device_id, com1.system_type, com1.edition )
+
+print(f"This is second computer details:", com2.device_name,com2.procesor, com2.device_id, com2.system_type, com2.edition )
+
+print(f"This is third computer details:", com3.device_name,com3.procesor, com3.device_id, com3.system_type, com3.edition )
+
+
+
