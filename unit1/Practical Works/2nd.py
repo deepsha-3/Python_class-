d99f2dbc6b76f7ab09e893e88c10b56fe31274bc
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-28 21:14:38 +0545

    update file

--- unit1/Practical Works/2nd.py
+++ unit1/Practical Works/2nd.py
@@ -4,7 +4,9 @@
 
 a = 10
 b= 5
-# Arithmetic Operators
+
+# 1. Arithmetic Operators
+
 print("Addition:", a + b)          # This is Addition Operator
 print("Subtraction:", a - b)       # This is Subtraction Operator   
 print("Multiplication:", a * b)    # This is Multiplication Operator
@@ -14,7 +16,7 @@ print("Exponentiation:", a ** b)   # This is Exponentiation Operator
 print("Floor Division:", a // b)   # This is Floor Division Operator
 
 
-# Comparison Operators
+# 2. Comparison Operators
 print( a == b)     # Equal to Operator
 print( a != b)     # Not equal to
 print( a > b)      # Greater than
@@ -22,13 +24,13 @@ print( a < b)      # Less than
 print( a >= b)     # Greater than
 print( a <= b)     # Less than or equal to
 
-# Logical Operators
+# 3. Logical Operators
 print("Logical AND:", a > 0 and b > 0)   # Logical AND
 print("Logical OR:", a > 0 or b < 0)     # Logical OR
 print("Logical NOT:", not (a > 0))       # Logical NOT
 
 
-# Assignment Operators
+# 4. Assignment Operators
 a += 5                        # Add and assign
 print("New value of a", a)  
 b -= 2                        # Subtract and assign
