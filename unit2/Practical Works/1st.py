53136e2ef01242d5c23fea580fddb1ca5761b1be
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-29 14:32:42 +0545

    add new file

---
+++ unit2/Practical Works/1st.py
@@ -0,0 +1,21 @@
+# Write program to apply if, match, break, continue statements for decision making
+
+# 1. If statement 
+age = 18
+if age >= 18:
+    print("You are eligible for voting.")
+print("")
+
+# 2. Match statement 
+score = 85
+match score:
+    case 90 | 100:
+        print("You got an A+")
+    case 80 | 89:
+        print("You got an A")
+    case _:
+        print("You got a B")
+print("")
+
+# 3. Break statement
+
