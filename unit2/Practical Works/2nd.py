c7272730372343ffc0a5e5da3f920f0f8d21d002
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-29 14:45:41 +0545

    new file

---
+++ unit2/Practical Works/2nd.py
@@ -0,0 +1,12 @@
+# Write program to utilize different loop statements to solve meaningful problems.
+
+# 1. For loop to print even numbers from 0 to 20 
+print("For loop to print even numbers from 0 to 20:")
+for i in range (0, 21):
+    if i % 2 == 0:
+        print(i)
+print("")
+
+# 2. While loop to calculate the sum of numbers from 1 to 100
+print("While loop to calculate the sum of numbers from 1 to 100:")
+
