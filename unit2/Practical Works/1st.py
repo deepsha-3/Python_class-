816c8aaec724ec9b64cff584dfeff10aa75cf538
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-29 14:38:39 +0545

    new file

--- unit2/Practical Works/1st.py
+++ unit2/Practical Works/1st.py
@@ -25,3 +25,11 @@ for i in range(20):
     if i == 10:
         break
     print(i)
+print("")
+
+# 4. Continue statement
+print("Continue statement:")
+for i in range(20):
+    if i == 10:
+        continue
+    print(i)
