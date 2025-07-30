e984a78f42b6084d3ed42465da4b5f7e80e13e18
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-30 06:57:28 +0545

    new file added

---
+++ unit 4/inheritance1.py
@@ -0,0 +1,17 @@
+# write a program apply inheritance where Vehicle class. 
+
+class Vehicle:
+    def drive(self):
+        print("Drive your vehicle.")
+
+    def honk(self):
+        print("honk !")
+
+class Car(Vehicle):
+    def color(self):
+        print("Explain your car color.")
+        
+car = Car()
+car.drive()
+car.honk()
+car.color()
--- unit2/Practical Works/4th.py
+++ unit2/Practical Works/4th.py
@@ -1,1 +1,3 @@
-# 
+# Write program to create a different patterns using nested loop.
+for i in range(1, 6): 
+    for a in 
