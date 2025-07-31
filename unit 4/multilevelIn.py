35b672153ef82554c51bb156977189c604088189
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-31 06:42:52 +0545

    new files

---
+++ unit 4/multilevelIn.py
@@ -0,0 +1,20 @@
+# multilevel inheritance example in Python:
+
+class Vehicle:
+    def color(self):
+        print("The color of the vehicle is Blue.")
+
+
+class Car(Vehicle):
+    def model(self):
+        print("The model of the vehicle is BMW X5.")
+
+class BMW(Car):
+    def speed(self):
+        print("The speed of the BMW X5 is 250 km/h.")
+
+v = BMW()
+v.color()
+v.model()
+v.speed()
+
