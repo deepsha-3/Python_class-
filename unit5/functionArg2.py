d40a8db18320d0aae018fe8b4064653c471fcb8e
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-01 06:49:16 +0545

    new files

--- unit5/functionArg2.py
+++ unit5/functionArg2.py
@@ -1,6 +1,6 @@
 # function with argument
 
-def std_details(rollnum, name, age):
+def std_details(rollnum, name, age):  # rollnum, name, age "parameter"
     print(f"Roll number: {rollnum}, Name: {name}, Age: {age}")
 
-std_details(1 , "Dipisha Dumre", 12)
+std_details(1 , "Dipisha Dumre", 12)  # 1, "Dipisha Dumre", 12 "Argument"
