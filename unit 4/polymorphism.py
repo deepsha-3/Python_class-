edf5dc93b680f2a6cad46f2590787663a5a07eb6
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-01 06:30:12 +0545

    new files

--- unit 4/polymorphism.py
+++ unit 4/polymorphism.py
@@ -1,1 +1,13 @@
 # polymorphism in python:
+class Car:
+    def ride(self):
+        print("Let's go for a ride the car.")
+
+class Bike:
+    def ride(self):
+        print("Let's go for a ride the bike.")
+
+class Auto:
+    def ride(self):
+        print("Let's go for a ride the auto.")
+
---
+++ unit5/functionwithArg.py
@@ -0,0 +1,6 @@
+# passing argument in python:
+
+def welcome(name):
+    print(f"Hello {name}, Welcome to the world of code.")
+
+welcome("Coders!")
