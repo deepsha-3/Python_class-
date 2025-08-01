7836ba0fa0f18ea0b6c6136de744d78b35e4da16
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-01 06:21:17 +0545

    new files

--- unit2/Practical Works/4th.py
+++ unit2/Practical Works/4th.py
@@ -1,6 +1,5 @@
 # Write program to create a different patterns using nested loop.
 for i in range(1, 6): 
     for a in range(1, i+1):
-        print("*" * i, end="")
+        print("*" * a, end="")
         print("")
-
