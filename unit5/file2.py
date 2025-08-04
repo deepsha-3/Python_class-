aa296eaac8bd694c4edd8cafc3cd6c624fe8aa2a
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-04 06:40:31 +0545

    new files

--- unit5/file2.py
+++ unit5/file2.py
@@ -1,4 +1,4 @@
-# file open in python:
+# file open in python
 file = open("unit5/file2.txt", "w")
 file.write("Hello coders, Welcome to the world of code. Here you can learn Python with me.\nYou can also explore my git and visit my github profile. \nNow I can't explain anything more explore by yourself.")
 file.close()
---
+++ unit5/file3.py
@@ -0,0 +1,5 @@
+# file by using for loop in python:
+file = open("unit5/file2.txt", "r")
+for line in file:
+    print(line)
+file.close()
