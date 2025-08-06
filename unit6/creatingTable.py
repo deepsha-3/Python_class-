2e69fbd62f291dfb50f182b021acc01252a1f9bb
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-06 10:10:16 +0545

    new file added

---
+++ unit6/creatingTable.py
@@ -0,0 +1,17 @@
+
+
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER
+)
+""")
+conn.commit()
+conn.close()
+
