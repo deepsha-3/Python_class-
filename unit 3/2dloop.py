78197c020a6fb21130c8a791ea285332f4596797
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-06 23:04:45 +0545

    new file

---
+++ unit 3/2dloop.py
@@ -0,0 +1,13 @@
+matrix=[
+    [1, 2, 3],    # this is first row (index 0)
+    [4, 5, 6],    # this is second row (index 1)
+    [7, 8, 9]     # this is third row (index 2)
+]
+
+
+# for row in matrix:
+#     print(row)
+
+for row in matrix:
+    for item in row:
+        print(item)
