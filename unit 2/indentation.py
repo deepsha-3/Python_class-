13d35b232ee60edd492ec1abb3caeab76832cdf2
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-06 23:00:11 +0545

    new file

---
+++ unit 2/indentation.py
@@ -0,0 +1,13 @@
+# This is the indentation example in Python
+    # Indentation is very important in Python as it defines the blocks of code.
+# a=8
+# if a>5:
+#     print("Big number")
+
+
+
+
+a=8
+if a>5:
+    print(a)   #in the indentation standard is 4 spaces
+   
