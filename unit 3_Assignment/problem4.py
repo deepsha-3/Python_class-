01f9b143dd05b7ced6347aa74d9af27579666a50
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-07 08:15:20 +0545

    new file

---
+++ unit 3_Assignment/problem4.py
@@ -0,0 +1,18 @@
+# Explain the use of in operator and slicing with examples. How is it different from indexing? 
+
+   # we can use "in" operator check the condition and value is exits or not in the same condition.
+
+# use of "in" operator
+list = [1, 32, 5, 7]
+print("This is used of in operator:",32 in list)
+
+     # slicing is extracting a part of string.   
+# use of slice 
+word="Python"
+print("Slice the word:",word[0:6])
+
+      # indexing is process of extraing a single element from sequence. 
+  # "in" operator can't use indexing but slice we use to excess a single value or element. 
+
+word = "Programming"
+print("Accessing the value by using index:",word[0])
