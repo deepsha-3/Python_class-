0a4cf4c762232720d05b9a7aa7653394b47f35ad
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-07 07:26:37 +0545

    new files

---
+++ unit 3/stringslicing.py
@@ -0,0 +1,16 @@
+# string slicing in python:
+
+word = "Python"
+# word_slice = slice(4,6)
+# print(word_slice)
+print(word)
+# print(word[4:6])
+
+# print "Pto"
+print(word[::2])
+
+# print "yhn"
+print(word[1::2])
+
+# print "n"
+print(word[5])
