296d85e0e9082ca5186098070d55e7c5e6fed590
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-07 07:12:00 +0545

    new files added

---
+++ unit 3/min.py
@@ -0,0 +1,9 @@
+# minimum value in a list
+
+list=[3,2,4,6,7, 55, 68, 0, 444, 100, 504, 4]
+
+print("First List:", list)
+
+# min value in list
+minimum= min(list)
+print("Minimum list value:", minimum)
