48bf4e0687bbfdda7723c81634e71429634f5e5f
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-07 07:09:06 +0545

    new files

---
+++ unit 3/listcomprenesion.py
@@ -0,0 +1,14 @@
+
+# first way to list comprehension
+
+
+squares = []
+for a in range(10):
+    squares.append(a*2)
+print(squares)
+
+
+
+# Second way to list comprehension
+# squares=[a*2 for a in range(21)]
+# print(squares)
