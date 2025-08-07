4916ef021410e6e0d4074828e84d621201ead112
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-07 08:10:18 +0545

    new file added

---
+++ unit 3_Assignment/problem2.py
@@ -0,0 +1,7 @@
+# Create a list of squares of all even numbers between 1 and 20 using list comprehension.
+
+squares = []  
+for a in range(1, 21):    # for loop 
+    if a% 2==0: # condition for even numbers
+         squares.append(a*a) 
+print(squares)   # print value 
