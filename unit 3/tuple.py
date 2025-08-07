3aa250cff667f3c39cd29fb78e28f5e98a2e3af6
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-07 08:07:08 +0545

    new files

---
+++ unit 3/tuple.py
@@ -0,0 +1,11 @@
+# tuple in python
+
+person=("Ram",25, "Kartik","Nepal", "Kartik")
+print(person[0])
+
+# tuple is an immuatuble, so we can't change any data or items.
+# person.append("Shyam")
+# print(person)
+
+print(person.index(25))
+print(person.count("Kartik"))
