0fa4991efa92ae35ac97dc21ffe0a7bb86c252d1
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-07 08:19:09 +0545

    new files

---
+++ unit 3_Assignment/problem6.py
@@ -0,0 +1,25 @@
+# Write a Python program to:
+    # • Create a dictionary with student names and their grades
+    # • Retrieve a grade
+    # • Add a new student
+    # • Remove an existing student
+
+
+# creating student details
+student_details = { 
+    "Dipisha": "A++",
+    "Dikshya": "A",
+    "Darshan": "B++"
+}
+
+# reteriving student grade
+print("The grade of Dipisha: ", student_details["Dipisha"])
+
+
+# add new student 
+student_details["Levent"] = "B" 
+print("New student add:", student_details)
+
+# remove student
+del student_details["Dikshya"]
+print("Remove Dikshya student from student_details:",student_details)
