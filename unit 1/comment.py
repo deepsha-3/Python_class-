23ed4b4dfbd04e5f5e87ca22dc12741a00825bd6
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-10 21:31:57 +0545

    new file

---
+++ unit 1/comment.py
@@ -0,0 +1,10 @@
+# single comment
+print("Hello, code") # This is single line comment
+
+
+
+"""
+This is 
+multiline"""  # This is multiline comment. Where use """....""" 
+
+print("Hello multiline comment")
