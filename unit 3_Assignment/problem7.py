3f460551d7761de4da15a213dcc6da3a9d21e6c0
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-10 18:20:39 +0545

    new file added

---
+++ unit 3_Assignment/problem7.py
@@ -0,0 +1,16 @@
+
+# What is a dictionary in Python? Write a program to count the frequency of each letter in a given string using a dictionary.
+
+letter = "Rewrite".lower()  # .lower() for converting letter uppercase and lowercase
+frequency_letter = {}  #empty dictionary
+
+for text in letter:
+    if text in frequency_letter:
+        frequency_letter[text] +=1 
+    else:
+        frequency_letter[text] =1
+
+
+for char, frequency in frequency_letter.items():
+    print(f"The '{char}' repeat in '{frequency}' times. ")
+
