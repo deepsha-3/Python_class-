ec3cd16ab12390539a39b61e4ffb9a9ad7aa07a8
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-10 21:12:50 +0545

    update file

---
+++ unit 1/2_Assignment.py
@@ -0,0 +1,12 @@
+#convert USD to NPR by using API 
+import requests
+name = input("Enter your name: ")
+
+USD = float(input("Enter amount in USD: "))
+url = "https://api.exchangerate-api.com/v4/latest/USD"
+response = requests.get(url)
+data = response.json()
+rate = data["rates"]["NPR"]
+
+NPR = USD * rate
+print(f"Hello {name}, ${USD} is equal to रू {NPR:.2f} NPR.")
