d3755a7daafe9d5c2211b199345bece12a4f53da
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-10 21:27:03 +0545

    new file

---
+++ unit 1/f_string3.py
@@ -0,0 +1,11 @@
+# f- string in python:
+
+price= 50
+qty=3
+print(f"The total cost is: Rs.{price*qty}")  # printing the value
+
+
+# f-string input from user 
+name=input("Enter your name:")
+food=input("Enter your favorite food:")
+print(f"Hey {name},I'll order some {food} for you.")
