9dbd2b399867e4fd98029486481bee42afb97786
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-10 18:57:06 +0545

    new file added

---
+++ unit 3_Assignment/problem9.py
@@ -0,0 +1,18 @@
+# Given a string "ProgrammingInPython", write Python statements to:
+      # • Slice out “Programming”
+      # • Reverse the whole string
+      # • Check if "Python" is in the string 
+
+text = "ProgrammingInPython"
+
+print("This is the original text:",text)
+   # slice out "Programming" from the word
+print("Slice the word from text:",text[0:11])
+    
+    # reverse the whole string 
+print("Reverse whole string of text:", text[::-1])
+
+    # check "Python" is string 
+print("Python is in the string?", "Python" in text)
+
+
