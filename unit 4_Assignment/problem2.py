c1d60490c8bd84a64c5a7461074c326365b3eccb
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-11 09:17:15 +0545

    new file added

---
+++ unit 4_Assignment/problem2.py
@@ -0,0 +1,12 @@
+# 2.Explain the purpose of the __init__() method in Python. Give an example using a Book class.
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+b = Book("The Psychology of Money", "Morgan Housel")
+b1 = Book("Atomic Habits","James Clear")
+
+print(f"First Book Details:{b.title},{b.author}")
+print(f"Second Book Details:{b1.title},{b1.author}")
