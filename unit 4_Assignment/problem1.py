dd8536536c23b976cd396d68599fd43cbc53f965
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-11 09:16:06 +0545

    new file added

---
+++ unit 4_Assignment/problem1.py
@@ -0,0 +1,17 @@
+# 1.Write a Python class called Laptop that has two attributes: brand and color. Create two objects with different values and print their details. 
+
+class Laptop: 
+    brand = ""   # class attribute
+    color = ""
+
+laptop1 = Laptop()  # object 1 
+laptop2 = Laptop()  # object 2
+
+laptop1.brand = "Dell"
+laptop1.color = "Black"
+
+laptop2.brand = "HP"
+laptop2.color = "Silver"
+
+print(f"This is the first laptop details: {laptop1.brand}, {laptop1.color}")
+print(f"This is the second laptop details: {laptop2.brand}, {laptop2.color}")
