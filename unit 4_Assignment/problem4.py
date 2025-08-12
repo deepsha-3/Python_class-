2a41b6d7064cc945b578d357620a4ffd8536d5a5
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-12 23:49:45 +0545

    new file added

---
+++ unit 4_Assignment/problem4.py
@@ -0,0 +1,13 @@
+# 4. Define a class Animal with a method speak(). Then define a child class Dog that inherits from Animal and adds a method bark(). Create a Dog object and call both methods. 
+
+class Animal:   # class
+    def speak(self):   # method 
+        print("Animal makes a sound.")
+
+class Dog(Animal):  # inherit class
+    def bark(self): # method
+        print("Dog barks")
+
+dog = Dog()   # object create
+dog.speak()
+dog.bark()
