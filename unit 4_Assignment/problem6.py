7b499d8c46484e8b215abe306dfd0a461d7d9097
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-12 23:51:03 +0545

    new file added

---
+++ unit 4_Assignment/problem6.py
@@ -0,0 +1,22 @@
+# 6.Write a Python program using multiple inheritance where:
+"""   •Chef class can cook
+      •Driver class can drive
+      •Employee class inherits both and can also attend meetings    """
+
+class Chef:
+    def cook(self):
+        print("Chef can cook somthing new.")
+
+class Driver:
+    def drive(self):
+        print("The driver can drive properly.")
+
+class Employee(Chef, Driver):
+    def meeting(self):
+        print("Employee can inheits both data from to different class.")
+
+
+e = Employee()
+e.cook()
+e.drive()
+e.meeting()
