4fbcb82d88da2310f1bda513e7e3f422993a4a0f
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-13 19:33:19 +0545

    update file

--- unit 4_Assignment/problem7.py
+++ unit 4_Assignment/problem7.py
@@ -1,4 +1,4 @@
-# 7.What is polymorphism in Python? Explain with an example where different classes have a method draw() and a function calls them.
+# 7. What is polymorphism in Python? Explain with an example where different classes have a method draw() and a function calls them.
 
 class Car:
     def draw(self):
