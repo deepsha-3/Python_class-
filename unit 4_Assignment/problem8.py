197d4c8db63320c20ea8d237e379d82fd0233f80
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-14 10:47:29 +0545

    update file

--- unit 4_Assignment/problem8.py
+++ unit 4_Assignment/problem8.py
@@ -1,1 +1,26 @@
 # 8. What is data hiding in Python? Explain with an example where a class BankAccount hides the balance and provides methods to access or modify it. 
+
+class BankAccount:
+    def __init__(self, first_balance):
+        self.__balance = first_balance
+
+    def get_balance(self):
+        return self.__balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print("Not enough balance.")
+account = BankAccount(1000)
+print("Balance:", account.get_balance())
+
+account.deposit(500)
+print("Balance after deposit:", account.get_balance())
+
+account.withdraw(300)
+print("Balance after withdrawal:", account.get_balance())
+
