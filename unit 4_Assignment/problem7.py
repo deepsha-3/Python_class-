8ff860b07a6ed51bdc879b74c2ebf35bc3b96e3f
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-14 21:38:11 +0545

    update file

--- unit 4_Assignment/problem7.py
+++ unit 4_Assignment/problem7.py
@@ -1,5 +1,25 @@
 # 7. What is polymorphism in Python? Explain with an example where different classes have a method draw() and a function calls them.
 
-class Car:
+class Pencil:
     def draw(self):
-        print("")
+        print("Drawing with pencil is easy.")
+
+class Brush:
+    def draw(self):
+        print("We can use brush for drawing.")
+
+class Color:
+    def draw(self):
+        print("Color use for drawing and painting picture.")
+
+def create_picture(tool):
+    tool.draw()
+
+p = Pencil()
+b = Brush()
+m = Color()
+
+create_picture(p)
+create_picture(b)
+create_picture(m)
+
