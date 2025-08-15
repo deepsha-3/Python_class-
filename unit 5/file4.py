77ae976df7435f6352619a89d9ee3b002da12977
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-15 19:20:29 +0545

    new file added

---
+++ unit 5/file4.py
@@ -0,0 +1,13 @@
+# file reading in python: 
+file = open("unit 5/file4.txt", "w")
+file.write("""Python is a versatile language loved by beginners and pros alike.
+Its syntax is clean, readable, and expressive, making code easier to write.
+Whether you're building websites, analyzing data, or automating tasks,
+Python helps you focus on logic instead of boilerplate syntax.
+It supports multiple paradigms—object-oriented, functional, and procedural.
+Libraries like NumPy, Django, and Flask supercharge your development speed.
+Community support is massive, with tutorials and forums everywhere.
+Debugging feels smoother thanks to intuitive error messages.
+You can prototype ideas quickly and scale them into real apps.
+In short, Python makes problem-solving elegant, efficient, and even fun.""")
+file.close()
