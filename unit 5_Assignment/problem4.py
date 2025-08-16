f846a475ab7b6989a0711942a2fd9dfc072190c8
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-16 15:55:22 +0545

    new file added

---
+++ unit 5_Assignment/problem4.py
@@ -0,0 +1,7 @@
+# 4. Write a function multiply(a, b) that returns the product of two numbers. Call the function and print the result.
+
+def multiply(a,b):
+    return a*b
+
+product = multiply(3,4)
+print("The product of two number is:",product)
