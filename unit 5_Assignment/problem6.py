9ebc2e797b33cc2a847404aacf42aca9ae952635
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-16 15:57:26 +0545

    new file added

---
+++ unit 5_Assignment/problem6.py
@@ -0,0 +1,7 @@
+# 6. Write a Python program to write the line "Learning Python is fun!" into a file named note.txt. 
+
+file = open("unit 5_Assignment/note.txt", "w")
+file.write("Learning Python is fun!")
+file.close()
+
+
