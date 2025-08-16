d7983977ba5e5acefa13d44d05c687bbf39e4239
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-16 15:53:44 +0545

    new file added

---
+++ unit 5_Assignment/problem1.py
@@ -0,0 +1,12 @@
+# 1. What is a function in Python? Explain how it helps in writing clean and reusable code.
+
+# function example:
+
+def add_num(a,b):
+    result = a+b
+    return result
+
+# using function
+sum = add_num(3,1)
+
+print("The sum of two numbers:", sum)
