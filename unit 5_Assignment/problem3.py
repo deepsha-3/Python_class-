8c76f18ca75ebf7803456daf82daa01cc4c5a3cc
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-16 15:54:46 +0545

    new file added

---
+++ unit 5_Assignment/problem3.py
@@ -0,0 +1,7 @@
+# 3. Create a function greet(name) that takes a name as argument and prints “Hello, [name]!”. Call it for two different names.
+
+def greet(name):
+   print(f"Hello, {name}.") 
+
+greet("Dipisha Dumre")
+greet("Dikshya Subedi")
