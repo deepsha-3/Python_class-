51cc9fffc18c856eadec109c7b6f58dc73337f3d
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-16 15:55:57 +0545

    new file added

---
+++ unit 5_Assignment/problem5.py
@@ -0,0 +1,17 @@
+# 5.Explain the difference between a function that prints a value and a function that returns a value. Give one example of each.
+
+# function print a value
+def greet():   # function create
+    print("Hello corders,Welcome to the world of code.")  # print a value
+
+greet()       # function call 
+
+
+# function return a value
+ 
+def add(p, q):        # function create 
+    return p + q      # return value 
+
+sum = add(400,105)    # provide value  
+
+print("The sum of the two numbers:", sum)    # print value 
