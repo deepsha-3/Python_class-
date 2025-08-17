d999359a6720ac27fec438bcaee0b94817b6b83c
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-17 18:36:13 +0545

    update file

--- unit 3/Practical Works/1st.py
+++ unit 3/Practical Works/1st.py
@@ -1,6 +1,6 @@
 # Write a program to create list and add elements in list, remove elements from list and display list items.
 
-# create listr
+# create list
 list =[1,4,5,6,7, 8]
 
 list.append(0)  # add elements
