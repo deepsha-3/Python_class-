f5c27aadfbe83f14a6427703022de19ba4283455
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-17 18:40:19 +0545

    update file

--- unit 3/Practical Works/1st.py
+++ unit 3/Practical Works/1st.py
@@ -1,7 +1,7 @@
 # Write a program to create list and add elements in list, remove elements from list and display list items.
 
 # create list
-list =[1,4,5,6,7, 8]
+list =[1, 4, 5, 6, 7, 8]
 
 list.append(0)  # add elements
 
