2bf4ea45170ab965df3385b6dd3f853fafcac4f4
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-17 18:59:35 +0545

    update file

--- unit 3/Practical Works/2nd.py
+++ unit 3/Practical Works/2nd.py
@@ -1,6 +1,6 @@
 # Write a program to make use of list slicing concept to display elements of list. 
 
-list = ['Dipisha', 'Karan', 'Dikshya', 'Kartik', '...1...', '....2...']
+list = ['Dipisha', 'Karan', 'Dikshya', 'Kartik', 'Levent', 'Abel']
 print("The original list is:", list)
 
 print("The list after slicing is:", list[0:4])
