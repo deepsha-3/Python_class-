24a7374be3750ba1ec53bdd253c8d4880c436a02
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-18 22:32:56 +0545

    new file added

---
+++ unit  2/elif.py
@@ -0,0 +1,23 @@
+# ef-else statement example:
+marks= int(input("Enter your marks:"))
+
+if(marks>=90):
+    print("Grade A+")
+
+elif(marks>=80):
+    print("Grade A")
+
+elif(marks>=70):
+    print("Grade B+")
+
+elif(marks>=60):
+    print("Grade C+")
+
+elif(marks>=50):
+    print("Grade C")
+
+elif(marks>=40):
+    print("Grade D")
+
+else:
+    print("Sorry!, You are fail.")
