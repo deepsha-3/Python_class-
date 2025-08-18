552499c9c8ab1ed54645400f291af02ffc106180
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-18 22:34:58 +0545

    new filw add

---
+++ unit  2/forloop.py
@@ -0,0 +1,13 @@
+# for loop in python 
+# for break
+# for num in range(0, 20):
+#     if(num==6):
+#         break
+#     print(num)
+
+
+# continue statement in for loop example in python:
+for num in range(0, 20):
+    if (num==6):
+        continue
+    print(num)
