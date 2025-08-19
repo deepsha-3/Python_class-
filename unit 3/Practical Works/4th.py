65b081dc247ddf78a3cd165821d54f6f7a9f6ee5
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-19 21:34:44 +0545

    update file

--- unit 3/Practical Works/4th.py
+++ unit 3/Practical Works/4th.py
@@ -1,3 +1,7 @@
 # Write a program to apply list comprehension. 
 
 # create a list of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# create a new list with squares of the numbers
+squares = [x**2 for x in numbers]
