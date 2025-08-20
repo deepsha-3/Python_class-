3917e91230ae90249c7bfeaf2b07b45a9062f8a8
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-20 22:28:46 +0545

    update file

--- unit 3/Practical Works/6th.py
+++ unit 3/Practical Works/6th.py
@@ -6,5 +6,6 @@ tuples = (1,3,4,5)
 print ("The original tuple is:", tuples)
 
 # add elements in tuple
-tuples =tuples + (4,6)
+tuples =tuples + (7,6)
 
+print ("The tuple after adding elements is:", tuples)
