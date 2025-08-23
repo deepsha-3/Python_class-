5eefa76fc84063fdcb9f65f3c5c816657aade1f8
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-23 22:32:46 +0545

    new file added

--- unit 3/Practical Works/8th.py
+++ unit 3/Practical Works/8th.py
@@ -1,3 +1,12 @@
 # Write a program to create a set, add elements in set, remove elements from set and display set items. 
 
 # create a set 
+sets = set()  # this is empty sets
+
+# add elements in set
+sets.add(0)
+sets.add(1)
+sets.add(2)
+sets.add(4)
+print("The items in a set are:",sets)
+
