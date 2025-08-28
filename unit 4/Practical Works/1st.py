330f1038282a9370b7345d85aa5f260fe56d049b
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-28 11:22:31 +0545

    new file added

--- unit 4/Practical Works/1st.py
+++ unit 4/Practical Works/1st.py
@@ -1,3 +1,7 @@
 # Write program to elaborate object oritented concept with simple examples. 
 
 # create class
+class Car:
+    def __init__(self,model, color):
+        self.model = model
+        self.color = color
