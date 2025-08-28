226df9e414929d06ed58ff42540158dd923a70f8
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-28 11:24:43 +0545

    new file added

--- unit 4/Practical Works/1st.py
+++ unit 4/Practical Works/1st.py
@@ -2,6 +2,7 @@
 
 # create class
 class Car:
-    def __init__(self,model, color):
+    def __init__(self,model, color, year):
         self.model = model
         self.color = color
+        self.year = year
