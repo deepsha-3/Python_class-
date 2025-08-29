0e8485cda1e1e25fa01cef14a1900287d7c6959d
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-29 09:39:15 +0545

    new file added

--- unit 4/Practical Works/2nd.py
+++ unit 4/Practical Works/2nd.py
@@ -1,3 +1,8 @@
 # Write program to make use of __init__ method to initialize objects.
 
 # create class 
+class Car:
+    def __init__(self, model, color, year):
+        self.model = model
+        self.color = color
+        self.year = year
