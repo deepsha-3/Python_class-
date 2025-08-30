4e62b124f172dc08796665f2850f13a127c95125
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-30 22:11:30 +0545

    new file added

--- unit 4/Practical Works/3rd.py
+++ unit 4/Practical Works/3rd.py
@@ -1,3 +1,15 @@
 # Write a program to apply different types of inheritance.
 
 # Single Inheritance
+
+class Car:
+    def drive(self):
+        print("Car is driving")
+
+class SportsCar(Car):
+    def race(self):
+        print("Sports car is racing")
+
+car = SportsCar()
+car.drive()
+car.race()
