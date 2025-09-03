b77b6dff98aa6e7ef00618d7335beee4ef5b60cc
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-03 21:15:42 +0545

    new file added

--- unit 4/Practical Works/4th.py
+++ unit 4/Practical Works/4th.py
@@ -1,3 +1,8 @@
 # Write a Program to elaborate polymorphism and data hiding concept. 
 
 # create a class 
+class Employee:
+    def __init__(self, name, salary):
+        self.name= name
+        self.salary= salary
+        
