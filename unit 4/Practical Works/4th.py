dd197aea4cd0ac2900136f25b70630083bbccf8c
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-03 21:38:09 +0545

    new file added

--- unit 4/Practical Works/4th.py
+++ unit 4/Practical Works/4th.py
@@ -4,8 +4,12 @@
       # create a class 
 class Animal: 
     def speak(self): 
-        print("Animal speaks")
+        print("Animal speaks.")
 
 class Dog(Animal):
     def speak(self):
         print("The dog barks")
+
+class Cat(Animal):
+    def speak(self):
+        print("The cat meows")
