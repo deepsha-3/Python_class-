32ca164b3a6f144d98a965a0fe3e04b606e0738d
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-09 20:25:54 +0545

    new file added

--- unit 5/Practical Works/3rd.py
+++ unit 5/Practical Works/3rd.py
@@ -1,6 +1,6 @@
 # Write program to store output in file. 
 
-f = open("output.txt", "w")
+f = open("output.txt", "w") 
 f.write("Hello, this is a sample output stored in a file.\n")
 f.write("This demonstrates writing output to a file in Python.\n")
 f.close()
