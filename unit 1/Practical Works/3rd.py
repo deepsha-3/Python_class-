08a37a22e3091eb14d45b23bb9fac910f9574658
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-07 16:28:50 +0545

    new file

--- unit 1/Practical Works/3rd.py
+++ unit 1/Practical Works/3rd.py
@@ -1,5 +1,5 @@
 # Write a program to make use of I/O functions.
 
-name = input("Enter your name:") #input
-city = input("Enter your city:") #input
-print(f"Hello, It's me {name} from {city}.") #output
+name = input("Enter your name:")              #input
+city = input("Enter your city:")              #input
+print(f"Hello, It's me {name} from {city}.")  #output
