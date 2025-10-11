26970251e843884e6a494c2313a0f40492924739
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-11 21:31:53 +0545

    new file

---
+++ unit 4/single_In.py
@@ -0,0 +1,13 @@
+# single inheritance python:
+
+class Car:  # create class
+    def color(self):
+        print("The color of the car is Black.")
+
+class BMW(Car): # create class BMW / drive class 
+    def model(self): 
+        print("The model of the car is BMW x5.")
+
+car=BMW() # create object 
+car.color() 
+car.model()
