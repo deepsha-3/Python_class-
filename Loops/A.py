96ef056d77a9e49aa5702af09ad3e5a0911ee696
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-21 20:45:11 +0545

    new file

--- Loops/A.py
+++ Loops/A.py
@@ -2,3 +2,10 @@
 # Creating A using a loop
 
 num = 5
+for i in range(num):
+    if i == 0:
+        print(' ' * (num - i - 1) + '*' + ' ' * (num - i - 1))
+    elif i == num // 2:
+        print('*' * (2 * num - 1))
+    else:
+        print(' ' * (num - i - 1) + '*' + ' ' * (2 * i - 1) + '*')
