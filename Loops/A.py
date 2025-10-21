2acd592efdfa22a437871d85ed2731095288ffd3
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-21 21:08:11 +0545

    new file

--- Loops/A.py
+++ Loops/A.py
@@ -1,17 +1,22 @@
 
 # Creating A using a loop
 
-num = 8  # Even number
+# num = 8
+# for i in range(num):
+#     if i == 0:
+#         print(' ' * (num - i - 1) + '*' + ' ' * (num - i - 1))
+#     elif i == num // 2:
+#         print('*' * (2 * num - 1))
+#     else:
+#         print(' ' * (num - i - 1) + '*' + ' ' * (2 * i - 1) + '*')
 
-for i in range(num):
-    if i == 0:
-        # Top point
-        print(' ' * (num - i - 1) + '*' + ' ' * (num - i - 1))
-    elif i == num // 2 - 1:
-        # Middle bar shifted up
-        print('*' * (2 * num - 1))
-    else:
-        # Diagonal arms
-        spaces_outside = num - i - 1
-        spaces_inside = 2 * i - 1
-        print(' ' * spaces_outside + '*' + ' ' * spaces_inside + '*')
+n= 5
+for i in range(n,-1,-1):                      
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(1,n+1):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+  
