4e5757728d8e81aba94653190b72c30a75020be8
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-06 23:23:33 +0545

    new file

--- unit 5/Practical Works/4th.py
+++ unit 5/Practical Works/4th.py
@@ -1,8 +1,2 @@
-# Write program to read input from file.
-f = open("input.txt", "r")
-data =f.read()
-print("Data read from file:")
-print(data)
-f.close()
 
-# this file can't work properly.
+# Write program to read input from file. 
