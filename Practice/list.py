97714879914685a78fd09aaba3641efa49cd54d0
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-09 13:39:12 +0545

    new code file

--- Practice/list.py
+++ Practice/list.py
@@ -1,2 +1,33 @@
 
 # List methods 
+
+List = ["a", "b", "c", "d"]
+print("Original List:", List)
+
+# Append method
+List.append("z")
+print("After Append:", List)
+
+# insert method
+List.insert(3, "p")
+print("After Insert:", List)
+
+# sort method
+List.sort()
+print("After Sort:", List)
+
+# remove method
+List.remove("a")
+print("After Remove:", List)
+
+# reverse method
+List.reverse()
+print("After Reverse:", List)
+
+# min value 
+Value = min(List)
+print("Min Value:", Value)
+
+# max value
+Value1 = max(List)
+print("Max Value:", Value1)
