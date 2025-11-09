18d9b2b3d9e9752204f66b4b062aa6bb7f26afb2
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-09 13:43:06 +0545

    new file

---
+++ Practice/list_slice.py
@@ -0,0 +1,11 @@
+
+# list slicing
+
+word = "Deepsha"
+print("Original Word:", word)
+
+# slicing the "Deep"
+print("slice the word: ", word[0:4] )
+
+# slicing the "sha"
+print("slice again the word: ", word[4:7] )
