d81c823394e3976757242a60d76c0243ef86ceb4
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-10 21:22:05 +0545

    new file added

--- Practice/singlein.py
+++ Practice/singlein.py
@@ -3,8 +3,8 @@
 
 class Car:
     def car_info(self):
-        print("This is a car")
+        print("This is a car.")
 
 class BMW(Car):
     def bmw_info(self):
-        print("This is a BMW car")
+        print("This is a BMW car.")
